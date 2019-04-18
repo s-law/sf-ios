d549299cad73becc0d182035ff64e86bb7dc2661
Roderic Campbell <roderic@gmail.com>
2019-04-18 13:23:53 -0700

    Setup the UI so that we can receive group content
    Add the collectionview to a new tableview cell type, create image
    cells for the collectionview

---
+++ SF iOS/SF iOS/GroupSelection/GroupSelectionTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  GroupSelectionTableViewCell.h
+//  Coffup
+//
+//  Created by Roderic Campbell on 4/18/19.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GroupSelectionTableViewCell : UITableViewCell
+@property (nonatomic, readonly) UICollectionView *collectionView;
+@end
+
+NS_ASSUME_NONNULL_END
